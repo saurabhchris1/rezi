b4207e033d5316354b48a833c900421c4a317749
Saurabh Jaiswal <saurabhchris1@gmail.com>
2020-11-17 00:00:33 -0800

    Created Base Rezi Template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Home from "./hoc/Home/Home";
+import Layout from "./hoc/Layout/Layout";
+import {Route, Switch, withRouter, Redirect} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Layout>
+        <Switch>
+          <Route path='/'exact component={Home}/>
+          <Redirect to='/'/>
+        </Switch>
+      </Layout>
   );
 }
 
---
+++ src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import {Typography} from "@material-ui/core";
+import CustomClasses from './Dashboard.module.css';
+import Resume from '../../assets/images/resume.png';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        height: 310,
+        width: 280,
+    },
+    control: {
+        padding: theme.spacing(2),
+    },
+}));
+
+
+const Dashboard = (props) =>{
+    const [spacing, setSpacing] = React.useState(2);
+    const classes = useStyles();
+
+    const handleChange = (event) => {
+        setSpacing(Number(event.target.value));
+    };
+
+    return (
+        <Grid container className={classes.root} spacing={2}>
+            <Grid item xs={12}>
+                <Grid container spacing={spacing}>
+                    <Grid key={1} item>
+                        <Button  variant="contained" color="primary">Resumes</Button>
+                    </Grid>
+                    <Grid key={2} item>
+                        <Button  variant="outlined">Cover Letters</Button>
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid item xs={12}>
+                <Grid container spacing={spacing}>
+
+                    <Grid key={1} item>
+                        <Paper className={CustomClasses.PaperDivDashedBorder} >
+                            <Typography variant="caption" display="block" gutterBottom> Create New Resume</Typography>
+                        </Paper>
+                    </Grid>
+                    <Grid key={2} item>
+
+                        <Paper className={CustomClasses.Content} >
+                            <img src={Resume} alt="Logo" className={CustomClasses.PaperDivImage}/>
+                        </Paper>
+                    </Grid>
+                    <Grid key={3} item>
+                        <Paper className={CustomClasses.Content} >
+                            <img src={Resume} alt="Logo" className={CustomClasses.PaperDivImage}/>
+                        </Paper>
+                    </Grid>
+
+                </Grid>
+            </Grid>
+
+        </Grid>
+    );
+}
+
+export default Dashboard;
